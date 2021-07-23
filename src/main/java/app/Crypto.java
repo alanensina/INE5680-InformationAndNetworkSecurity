0be050e0b1ff4274c734f0a11754f83ed2b3c85f
Alan Ensina <aensina@uolinc.com>
2021-07-23 13:39:45 -0300

    Starting project with maven dependencies

---
+++ src/main/java/app/Crypto.java
@@ -0,0 +1,13 @@
+package app;
+
+import model.User;
+
+public class Crypto {
+    public static void main(String[] args) {
+
+        User user = new User("User");
+        System.out.println(user.toString());
+
+
+    }
+}
